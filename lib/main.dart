f6d186b0b9f5b9c034dae3743770450912899a9a
Luke Greenwood <luke@Lukes-MacBook-Pro-2.local>
2020-02-29 15:58:56 +0000

    Update readme

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Base BLoC State Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
